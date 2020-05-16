b957df58528f856a0119fe83f1725fe64ed50343
gshanbhag525 <17478096+gshanbhag525@users.noreply.github.com>
2020-05-17 00:21:15 +0530

    add

--- hello.go
+++ hello.go
@@ -1,16 +1,8 @@
 package main
 
 import "fmt"
-import "strconv"
 
 func main() {
 
-	var a int = 12
-	fmt.Printf("%v, %T", a, a)
-
-	var b string
-	b = strconv.Itoa(a)
-	fmt.Printf(" %v, %T", b, b)
-
-
+	fmt.Println("Hello Gunesh!!")
 }
---
+++ stringConversions.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	var a int = 12
+	fmt.Printf("%v, %T", a, a)
+
+	var b string
+	b = strconv.Itoa(a)
+	fmt.Printf(" %v, %T", b, b)
+
+}
