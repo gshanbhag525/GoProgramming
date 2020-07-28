cbfa0bbd77ba4acead0da2dc89bfbb232761d787
gshanbhag525 <17478096+gshanbhag525@users.noreply.github.com>
2020-07-29 00:20:26 +0530

    🎼 Composition in Go

--- structs/structs.go
+++ structs/structs.go
@@ -1,20 +1,49 @@
+// package main
+
+// import "fmt"
+
+// // type Celebrity struct {
+// // 	Age int
+// // 	CelebrityName string
+// // 	Movies []string
+// // 	CoActors []string
+// // }
+
+// func main(){
+
+// 	aCelebrity := struct{age int} {age : 55}
+// 	bCelebrity := &aCelebrity
+// 	bCelebrity.age = 95 
+// 	fmt.Println(aCelebrity)
+// 	fmt.Println(bCelebrity)
+
+// }
+
+
+//################## Composition in Golang ########
+
 package main
 
 import "fmt"
 
-// type Celebrity struct {
-// 	Age int
-// 	CelebrityName string
-// 	Movies []string
-// 	CoActors []string
-// }
+type Vehicle struct {
+	Name string
+	Type string
+}
 
-func main(){
+type Car struct {
+	Vehicle
+	Maxspeed float32
+	Fueltype string
+}
 
-	aCelebrity := struct{age int} {age : 55}
-	bCelebrity := &aCelebrity
-	bCelebrity.age = 95 
-	fmt.Println(aCelebrity)
-	fmt.Println(bCelebrity)
+func main() {
+	c := Car{
+		Vehicle : Vehicle{Name : "Ferrari", Type : "Convertible" },
+		Maxspeed : 250,
+		Fueltype : "Diesel",
+	}
+
+	fmt.Println(c)
 
 }
