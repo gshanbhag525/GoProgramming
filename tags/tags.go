649da4093ff0d91ad873736496ef54783f1f150e
gshanbhag525 <17478096+gshanbhag525@users.noreply.github.com>
2020-08-02 23:28:52 +0530

    🏷  tags in struct golang

--- tags/tags.go
+++ tags/tags.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+type User struct {
+	Username string `maxsize:"20"`
+	Password string `minsize:"8"`
+}
 
 func main() {
-	fmt.Println("Tags : ")
+	u := User{}
+	t := reflect.TypeOf(u)
+	// field := t.Field(0) // alternative
+	field, _ := t.FieldByName("Password")
+	fmt.Println(field.Tag)
+	// fmt.Println(field.Tag.Get("maxsize"))
+
 }
